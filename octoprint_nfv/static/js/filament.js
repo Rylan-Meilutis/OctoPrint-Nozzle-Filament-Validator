eb41d39fe3bf43430f788b83cd59eb6654cc3f9e
Rylan Meilutis <rylan.meilutis@gmail.com>
2024-03-04 14:20:25 -0500

    prevent excess requests from the event handler binding multiple times

--- octoprint_nfv/static/js/filament.js
+++ octoprint_nfv/static/js/filament.js
@@ -1,4 +1,7 @@
 function setRefreshButtons() {
+    // Remove any existing event handlers for the button
+    $('#extruder-tabs').off('click', '[id^="refresh-filament-button"]');
+
     // Event delegation for refreshing the filament type
     $('#extruder-tabs').on('click', '[id^="refresh-filament-button"]', function () {
         displayData();
